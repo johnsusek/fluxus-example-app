3fe97478e9c1449c06224b97ecf1b6b2efb27b03
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2019-12-23 13:43:08 +0330

    initialize UIWindow with window scene instead of frame

--- FluxusExampleApp/SceneDelegate.swift
+++ FluxusExampleApp/SceneDelegate.swift
@@ -6,9 +6,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    let window = UIWindow(frame: UIScreen.main.bounds)
-    window.rootViewController = UIHostingController(rootView: ContentView().environmentObject(rootStore))
-    self.window = window
-    window.makeKeyAndVisible()
+    if let windowScene = scene as? UIWindowScene {
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIHostingController(rootView: ContentView().environmentObject(rootStore))
+        self.window = window
+        window.makeKeyAndVisible()
+    }
   }
 }
